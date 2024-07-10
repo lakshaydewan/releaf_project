5f73a534ffd62ee71173d3c3364f6ffc9c05b426
naman diwan <jaun@namans-MacBook-Air.local>
2024-07-10 14:56:13 +0530

    fixed studylayoutpage

--- src/pages/main pages/studylayout.jsx
+++ src/pages/main pages/studylayout.jsx
@@ -4,9 +4,9 @@ import { Navbar } from "../../components/navbar"
 
 function Studylyout() {
 
-    return <div>
+    return <div className="flex flex-col">
         <Navbar />
-        <div className="">
+        <div className="mt-12">
         <div className="flex justify-between">
         <div className="w-[900px] p-3 h-screen flex flex-col space-y-4">
             <iframe className="w-full h-[495px] border border-none rounded-3xl" src="https://www.youtube.com/embed/z5yvZW8Ep-E?si=7SvG7BW7OH2n54nE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -18,7 +18,7 @@ function Studylyout() {
                 </div>
             </div>
         </div>
-        <div className="min-w-96  w-2/5 h-screen grow py-3 ">
+        <div className="min-w-96 w-2/5 h-screen grow py-3 mt-2">
         <iframe src="src\assets\pdfs\Untitled document.pdf#toolbar=0&view=FitH" className="h-full w-full border-y-4 border-gray-700"></iframe>
         </div>
     </div>
