42de70f772524112f8f516cb64876a7b756797f4
LAKSHAY PUNIA <148634500+Lakshay-punia@users.noreply.github.com>
2024-07-11 17:05:38 +0530

    changed assignment cards component added pin component

--- src/components/assigmentcards.jsx
+++ src/components/assigmentcards.jsx
@@ -1,13 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { PinContainer } from "./pin";
+import exp from "constants";
 
 function Assigenmentscards(){
 
-    return <div className="w-full space-y-8 flex justify-center">
-       <div id="content div" className="w-10/12 flex flex-col items-center space-y-8">
+    return <div className=" w-full space-y-8  flex justify-center h-fit items-center">
+       <div id="content div" className="w-10/12 flex  flex-col items-center space-y-8">
             <div id="upper div" className="w-full flex justify-between">
                 <div id="left section" className="w-1/2">
                     <div id="text" className="w-4/5">
-                        <div className="text-3xl text-white font-bold">Your Source for Ready-made Assignments</div>
+                        <div className="text-5xl text-white font-bold">Your Source for Ready-made Assignments</div>
                     </div>
                     <div id="image or video "></div>
                 </div>
@@ -16,11 +18,59 @@ function Assigenmentscards(){
                 </div>
             </div>
             <div id="lower div" className="flex w-full justify-between">
-                <div className="h-96 bg-blue-500 min-w-20% w-5/12 border border-none rounded-3xl" style={{transition : "width 0.3s ease"}}></div>
-                <div className="h-96 bg-blue-700 min-w-20% w-3/12 border border-none rounded-3xl hover:w-3/5 hover:min-w-60%" style={{transition : "all 0.5s ease"}}></div>
-                <div className="h-96 bg-blue-500 min-w-20% w-3/12 border border-none rounded-3xl hover:w-3/5 hover:min-w-60%" style={{transition : "all 0.5s ease"}}></div>
+                <div className="w-4/12"><PinContainer
+                    title="/assignmets"
+                    href="/assigment/assigment"
+                >
+                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                    <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                        Aceternity UI
+                    </h3>
+                    <div className="text-base !m-0 !p-0 font-normal">
+                        <span className="text-slate-500 ">
+                        Customizable Tailwind CSS and Framer Motion Components.
+                        </span>
+                    </div>
+                    <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+                    <img src="src\assets\images\Screenshot 2024-07-04 230455.png" alt="" />
+                    </div>
+                </PinContainer></div>
+                <div className="w-4/12"><PinContainer
+                    title="/practical-file"
+                    href="/assigment/practicalfile"
+                >
+                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                    <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                        Aceternity UI
+                    </h3>
+                    <div className="text-base !m-0 !p-0 font-normal">
+                        <span className="text-slate-500 ">
+                        Customizable Tailwind CSS and Framer Motion Components.
+                        </span>
+                    </div>
+                    <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+                    <img src="src\assets\images\Screenshot 2024-07-04 230455.png" alt="" />
+                    </div>
+                </PinContainer></div>
+                <div className="w-4/12"><PinContainer
+                    title="/EG-sheets"
+                    href="/assigment/egsheets"
+                >
+                    <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+                    <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                        Aceternity UI
+                    </h3>
+                    <div className="text-base !m-0 !p-0 font-normal">
+                        <span className="text-slate-500 ">
+                        Customizable Tailwind CSS and Framer Motion Components.
+                        </span>
+                    </div>
+                    <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+                    <img src="src\assets\images\Screenshot 2024-07-04 230455.png" alt="" />
+                    </div>
+                </PinContainer></div>
             </div>
-       </div>
+        </div>
     </div>
 }
 
