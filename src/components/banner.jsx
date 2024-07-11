0266313ed9ad4cc066738546665cfb6d28ce319f
LAKSHAY PUNIA <148634500+Lakshay-punia@users.noreply.github.com>
2024-07-11 22:04:14 +0530

    added grid to banner background

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,8 +1,11 @@
 import { motion, useInView } from "framer-motion";
 import { Movingborderbutton } from "./movingborder";
+import { GridBackground } from "./gridbackground";
 
 function Banner(){
-    return <motion.div initial={{
+    return (<div>
+        <GridBackground />
+        <motion.div initial={{
         opacity: 0,
         scale : 0
     }} 
@@ -14,16 +17,24 @@ function Banner(){
         duration : 1,
     }}
     whileInView={{ opacity: 1 }}
-    className=" h-screen w-full p-20 flex justify-center">
+    className=" h-screen w-full relative z-5 flex justify-center">
                 <div className="flex w-1/2 text-4xl font-extrabold text-center text-white"><div><span className="text-blue-300">"Empowering</span> Your Academic Journey with Resources, News, and More!"</div>
-                <Movingborderbutton
-        borderRadius="1.75rem"
-        className="bg-black dark:bg-slate-900 text-white dark:text-white border-neutral-200 dark:border-slate-800"
-      >
-        Borders are cool
-      </Movingborderbutton>
                 </div>
             </motion.div>
+    </div>
+    )
 }
 
 export {Banner}
+
+<div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
---
+++ src/components/gridbackground.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+ 
+export function GridBackground() {
+  return (
+    <div className="h-screen w-full absolute dark:bg-black bg-black  dark:bg-grid-white/[0.2] bg-grid-white/[0.2] flex items-center justify-center absolute top-0 left-0 ">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+    </div>
+  );
+}
